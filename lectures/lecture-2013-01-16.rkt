71a1503fa81544984a3d8bd3f87780d4c017520c
Carl Eastlund <cce@racket-lang.org>
2013-01-16 09:08:39 -0500

    Starting lecture on 1/16 from where we left off on 1/14.

---
+++ lectures/lecture-2013-01-16.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(require "../software/cs4800.rkt" rackunit)
+
+(current-print pretty-print-handler)
+
+(define (faster-sort stuff)
+  (define N (length stuff))
+  (cond
+    [(<= N 1) stuff]
+    [else
+     (define N/2 (quotient N 2))
+     (define both-halves (bifurcate-list stuff N/2))
+     (define first-half (first both-halves))
+     (define second-half (second both-halves))
+     (define sorted-first-half (faster-sort first-half))
+     (define sorted-second-half (faster-sort second-half))
+     ???]))
