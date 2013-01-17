1908b525f94047f77bbb2282b1432aa6f0dadd4b
Carl Eastlund <cce@racket-lang.org>
2013-01-17 10:26:15 -0500

    Measured costs.

--- lectures/lecture-2013-01-17.rkt
+++ lectures/lecture-2013-01-17.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-(require "../software/cs4800.rkt" rackunit)
+(require "../software/cs4800.rkt" "../software/big-o.rkt" rackunit)
 
 (current-print pretty-print-handler)
 
-(define (faster-sort stuff)
-  (define N (length stuff))
+(define/cost (faster-sort stuff)
+  (define N (len stuff))
   (cond
     [(<= N 1) stuff]
     [else
@@ -19,8 +19,13 @@
        (combine-sorted-lists sorted-first-half sorted-second-half))
      sorted]))
 
+(define/cost (len stuff)
+  (cond
+    [(empty? stuff) 0]
+    [else (add1 (len (rest stuff)))]))
+
 ;; bifurcate-list : (Listof X) Number -> (list (Listof X) (Listof X))
-(define (bifurcate-list stuff size-of-first-half)
+(define/cost (bifurcate-list stuff size-of-first-half)
   (cond
     [(zero? size-of-first-half) (list empty stuff)]
     [else
@@ -35,7 +40,7 @@
 (check-equal? (bifurcate-list (list 4 3 2 1) 2) (list (list 4 3) (list 2 1)))
 
 ;; combine-sorted-lists : (Listof X) (Listof X) -> (Listof X)
-(define (combine-sorted-lists one two)
+(define/cost (combine-sorted-lists one two)
   (cond
     [(empty? one) two]
     [(empty? two) one]
@@ -55,3 +60,63 @@
 (check-equal?
   (faster-sort (list 2 8 7 3 5 4 1 0))
   (list 0 1 2 3 4 5 7 8))
+
+;; sort : (Listof Number) -> (Listof Number)
+;; Sorts the input list in ascending order.
+(define/cost (sort stuff)
+  (if (empty? stuff)
+    stuff
+    (put-into-stuff (first stuff)
+      (sort (rest stuff)))))
+
+;; put-into-stuff : Number (Listof Number)
+;; Put one thing into a bunch of sorted stuff.
+(define/cost (put-into-stuff thing stuff)
+  (if (empty? stuff)
+    (cons thing empty)
+    (if (< thing (first stuff))
+      (cons thing stuff)
+      (cons (first stuff)
+        (put-into-stuff thing (rest stuff))))))
+
+(define (best-case i) (build-list i identity))
+(define (worst-case i) (reverse (best-case i)))
+
+(current-cost-model
+  (cost-model
+    cons 1
+    first 1
+    second 2
+    rest 1
+    empty? 1
+    list (lambda xs (length xs))
+    < 1
+    <= 1
+    quotient 1
+    zero? 1
+    sub1 1
+    add1 1))
+
+(for/list {[i (list 10 20 30 40 50)]}
+  (define xs (worst-case i))
+  (list
+    i
+    (* i (log i))
+    (cost-of (faster-sort xs))
+    (cost-of (sort xs))))
+
+(for/list {[i (list 10 20 30 40 50)]}
+  (define xs (best-case i))
+  (list
+    i
+    (cost-of (faster-sort xs))
+    (cost-of (sort xs))))
+
+(define (cost-of-merge-sort n)
+  (define xs (worst-case n))
+  (cost-of (faster-sort xs)))
+
+(define (n-log-n n)
+  (* n (log n)))
+
+(O? cost-of-merge-sort n-log-n 25 10 random-small-number)
