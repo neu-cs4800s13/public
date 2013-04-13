054d486b4aa6609d6475acde042ebf1aede6d891
Carl Eastlund <cce@racket-lang.org>
2013-04-12 20:44:57 -0400

    Simple recursive implementation of difference.

--- homework/4/sample-solution.rkt
+++ homework/4/sample-solution.rkt
@@ -85,6 +85,9 @@
   (define table (make-multivector (list m n) #false))
   (difference-between table a 0 b 0))
 
+(define (difference/recursive a b)
+  (difference-between/recursive a 0 b 0))
+
 (define (difference-between table source i target j)
   (define result (multivector-ref table (list i j)))
   (cond
@@ -104,19 +107,48 @@
               (cost-to-copy table source i target j)
               (cost-to-swap table source i target j)))]))]))
 
+(define (difference-between/recursive source i target j)
+  (cond
+    [(and (= i (string-length source)) (= j (string-length target))) 0]
+    [(= i (string-length source)) (cost-to-insert/recursive source i target j)]
+    [(= j (string-length target)) (cost-to-delete/recursive source i target j)]
+    [else
+     (best
+       (list
+         (cost-to-replace/recursive source i target j)
+         (cost-to-insert/recursive source i target j)
+         (cost-to-delete/recursive source i target j)
+         (cost-to-copy/recursive source i target j)
+         (cost-to-swap/recursive source i target j)))]))
+
 (define (cost-to-replace table source i target j)
   (add1 (difference-between table source (+ 1 i) target (+ 1 j))))
+
+(define (cost-to-replace/recursive source i target j)
+  (add1 (difference-between/recursive source (+ 1 i) target (+ 1 j))))
 
 (define (cost-to-insert table source i target j)
   (add1 (difference-between table source i target (+ 1 j))))
 
+(define (cost-to-insert/recursive source i target j)
+  (add1 (difference-between/recursive source i target (+ 1 j))))
+
 (define (cost-to-delete table source i target j)
   (add1 (difference-between table source (+ 1 i) target j)))
+
+(define (cost-to-delete/recursive source i target j)
+  (add1 (difference-between/recursive source (+ 1 i) target j)))
 
 (define (cost-to-copy table source i target j)
   (cond
     [(char=? (string-ref source i) (string-ref target j))
      (difference-between table source (+ 1 i) target (+ 1 j))]
+    [else #false]))
+
+(define (cost-to-copy/recursive source i target j)
+  (cond
+    [(char=? (string-ref source i) (string-ref target j))
+     (difference-between/recursive source (+ 1 i) target (+ 1 j))]
     [else #false]))
 
 (define (cost-to-swap table source i target j)
@@ -127,6 +159,16 @@
        (char=? (string-ref source i) (string-ref target (+ 1 j)))
        (char=? (string-ref source (+ 1 i)) (string-ref target j)))
      (add1 (difference-between table source (+ 2 i) target (+ 2 j)))]
+    [else #false]))
+
+(define (cost-to-swap/recursive source i target j)
+  (cond
+    [(and
+       (<= (+ 2 i) (string-length source))
+       (<= (+ 2 j) (string-length target))
+       (char=? (string-ref source i) (string-ref target (+ 1 j)))
+       (char=? (string-ref source (+ 1 i)) (string-ref target j)))
+     (add1 (difference-between/recursive source (+ 2 i) target (+ 2 j)))]
     [else #false]))
 
 (define (best xs)
@@ -141,6 +183,15 @@
     [else (min x y)]))
 
 (module+ test
+  (check-equal? (difference/recursive "cat" "cat") 0)
+  (check-equal? (difference/recursive "bat" "cat") 1)
+  (check-equal? (difference/recursive "at" "cat") 1)
+  (check-equal? (difference/recursive "bat" "at") 1)
+  (check-equal? (difference/recursive "rat" "art") 1)
+  (check-equal? (difference/recursive "rats" "art") 2)
+  (check-equal? (difference/recursive "cab" "abc") 2)
+  (check-equal? (difference/recursive "pot" "top") 2)
+  (check-equal? (difference/recursive "rats" "tart") 3)
   (check-equal? (difference "cat" "cat") 0)
   (check-equal? (difference "bat" "cat") 1)
   (check-equal? (difference "at" "cat") 1)
